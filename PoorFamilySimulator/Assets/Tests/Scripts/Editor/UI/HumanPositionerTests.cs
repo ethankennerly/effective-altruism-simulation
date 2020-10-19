67bc7e687da58a38845b7e3a9a68e94f8a0e7f0a
Ethan Kennerly <ethankennerly@gmail.com>
2020-10-18 17:16:35 -0700

    Tests setting an average human position halfway.

--- PoorFamilySimulator/Assets/Tests/Scripts/Editor/UI/HumanPositionerTests.cs
+++ PoorFamilySimulator/Assets/Tests/Scripts/Editor/UI/HumanPositionerTests.cs
@@ -17,5 +17,35 @@ namespace PoorFamily.UI.Tests
             humanPositioner.UpdatePositions(simulator);
             Assert.AreEqual(new List<GameObject>(), humanPositioner.HumanObjects);
         }
+
+        [Test]
+        public void AveragePosition_6400And1600ThenAddTime_LerpHalfwayFromPoorestToRichest()
+        {
+            Simulator simulator = new Simulator();
+            HumanPositioner humanPositioner = new HumanPositioner();
+            humanPositioner.HumanPositionRoot = new GameObject().transform;
+            humanPositioner.PoorestPosition = new GameObject().transform;
+            humanPositioner.RichestPosition = new GameObject().transform;
+            humanPositioner.AveragePosition = new GameObject().transform;
+
+            SetLocalX(humanPositioner.PoorestPosition, -6f);
+            SetLocalX(humanPositioner.RichestPosition, 4f);
+            SetLocalX(humanPositioner.HumanPositionRoot, 1f);
+
+            Human income6400 = new Human{Income = 6400};
+            Human income1600 = new Human{Income = 1600};
+            simulator.Humans.Add(income6400);
+            simulator.Humans.Add(income1600);
+            simulator.AddTime(0.001f);
+            humanPositioner.UpdatePositions(simulator);
+            Assert.AreEqual(2f, humanPositioner.AveragePosition.localPosition.x);
+        }
+
+        private static void SetLocalX(Transform transform, float nextX)
+        {
+            Vector3 localPosition = transform.localPosition;
+            localPosition.x = nextX;
+            transform.localPosition = localPosition;
+        }
     }
 }
