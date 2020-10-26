f289a516de55b0117b358e2fa8f1f88c8adf4295
Ethan Kennerly <ethankennerly@gmail.com>
2020-10-26 08:42:35 -0700

    Donor funds GiveDirectly or saves in 9 tests.

--- PoorFamilySimulator/Assets/Scripts/Simulation/Donation/Donor.cs
+++ PoorFamilySimulator/Assets/Scripts/Simulation/Donation/Donor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PoorFamily.Simulation.Donation
@@ -6,19 +7,53 @@ namespace PoorFamily.Simulation.Donation
     [Serializable]
     public sealed class Donor
     {
-        [Range(0f, 100000f)] public float DisposableIncome;
+        [Range(0f, 100000f)] public float DisposableIncome = 30000f;
         public float FundsAvailable;
+        public ADonorOption SelectedOption;
 
         public DonorOptionMenu OptionMenu = new DonorOptionMenu();
 
+        public readonly List<ADonorOption> Options;
+
+        public Donor()
+        {
+            Options = new List<ADonorOption>();
+            Options.Add(OptionMenu.Save);
+            Options.Add(OptionMenu.GiveDirectly);
+        }
+
         public void Select(ADonorOption donorOption)
         {
-            UnityEngine.Debug.Log("TODO: Select: " + donorOption);
+            SelectedOption = donorOption;
         }
 
         public void AddYears(float deltaYears)
         {
-            UnityEngine.Debug.Log("TODO: Add Years: " + deltaYears);
+            FundsAvailable += deltaYears * DisposableIncome;
+
+            TryFundOption();
+        }
+
+        private void TryFundOption()
+        {
+            foreach(ADonorOption option in Options)
+            {
+                if (option != SelectedOption)
+                {
+                    option.FundingProgress = 0f;
+                    option.Funded = false;
+                    continue;
+                }
+
+                option.FundingProgress = FundsAvailable / option.Cost; 
+                option.Funded = option.FundingProgress >= 1f;
+                if (!option.Funded)
+                {
+                    continue;
+                }
+
+                FundsAvailable -= option.Cost;
+            }
         }
     }
 }
